f325ec92169bb8e23cc5fd825049cdde183c69bf
iemong <kiyoshidainagon@gmail.com>
2023-02-23 22:55:52 +0900

    fix rule

--- index.js
+++ index.js
@@ -11,5 +11,8 @@ module.exports = {
   extends: ["airbnb-base", "eslint:recommended", "prettier"],
   rules: {
     "no-console": 1,
+    "import/no-default-export": 2,
+    "import/prefer-default-export": 0,
+    "import/order": 2,
   },
 }
